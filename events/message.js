cc0a3a8c5d533444f8d03f9c22c5e8c7f52351d0
randair0x <39476773+randair0x@users.noreply.github.com>
2020-08-13 12:55:03 -0500

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,6 +1,15 @@
 module.exports = (client, message) => {
 	if (message.content.startsWith('$voyage')) {
-		let commr = require(`./../commands/${message.content.substring(8)}.js`);
+		let commr = require(`./../commands/${message.content.substring(8, message.content.indexOf(' ', 8))}.js`);
+		/*
+		pads so diff cmd lengths are accepted
+		$voyager!ahoy!...
+		$voyager!captain!...
+		options (-`option`) are processed in the command file itself
+		this also means i'll have to put another function for cmd && cmd,
+		but I really just care abt how does my child performs ;~;
+		*/
 		commr(message.channel, message, client);
 	}
 };
+
