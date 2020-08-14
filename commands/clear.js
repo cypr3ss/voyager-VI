91a3404a8e12bd29db07091d84fcb9eb023a5586
randair0x <39476773+randair0x@users.noreply.github.com>
2020-08-13 19:41:50 -0500

    Delete clear.js

--- commands/clear.js
+++
@@ -1,9 +0,0 @@
-module.exports = (channel, message) => {
-	if (message.content.substring()) {
-		let lengther = 0;
-		channel.fetchMessages().then(messages => {
-			lengther = Array.from(messages).length - 1;
-		});
-		channel.bulkDelete(lengther);
-	}
-};
